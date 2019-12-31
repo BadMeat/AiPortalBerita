44b4e39a1b6d3e2ac1d1f6059db5adf5be148c81
arif <arif@gitlab.lab>
2019-12-31 16:02:58 +0700

    Loading Image

---
+++ app/src/main/java/com/dolan/aiportalberita/Utils.kt
@@ -0,0 +1,31 @@
+package com.dolan.aiportalberita
+
+import android.content.Context
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+
+private fun getProgressDrawable(context: Context): CircularProgressDrawable {
+    return CircularProgressDrawable(context).apply {
+        strokeWidth = 10f
+        centerRadius = 50f
+        start()
+    }
+}
+
+fun ImageView.loadImage(url: String?, progressDrawable: CircularProgressDrawable) {
+    val option = RequestOptions()
+        .placeholder(progressDrawable)
+        .error(R.drawable.ic_broken_image)
+    Glide.with(context)
+        .setDefaultRequestOptions(option)
+        .load(url)
+        .into(this)
+}
+
+@BindingAdapter("android:imageUrl")
+fun loadImage(view: ImageView, url: String?) {
+    view.loadImage(url, getProgressDrawable(view.context))
+}
